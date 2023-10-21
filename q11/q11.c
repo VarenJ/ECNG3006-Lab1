b20b7bab9597e43d6d21411af236f8063f3746d5
VarenJ <145804375+VarenJ@users.noreply.github.com>
2023-10-20 20:59:59 -0400

    Add files via upload

---
+++ q11/q11.c
@@ -0,0 +1,25 @@
+/*
+ * File:   lab1.c
+ * Author: ECNG2005
+ *
+ * Created on October 18, 2023, 5:17 PM
+ */
+
+
+#include <xc.h>
+#define _XTAL_FREQ 4000000
+
+void main(void) {
+    TRISDbits.TRISD2=0;
+    LATDbits.LATD2=1;
+   
+    while(1==1)
+    {
+        LATDbits.LATD2=0;
+        __delay_ms(1000);
+         LATDbits.LATD2=1;
+        __delay_ms(1000);
+    }
+       
+      return;  
+    }
